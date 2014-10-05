21255380028151a459b4d0013821c3647f02d0aa
Hyunsik Choi <hyunsik@apache.org>
2014-10-04 17:17:11 -0700

    TAJO-1092: Improve the function system to allow other function implementation types.
    Conflicts:
    	tajo-core/src/main/java/org/apache/tajo/engine/codegen/EvalCodeGenerator.java
    	tajo-core/src/main/java/org/apache/tajo/engine/codegen/TajoGeneratorAdapter.java
    	tajo-core/src/test/java/org/apache/tajo/engine/eval/ExprTestBase.java

--- tajo-core/src/main/java/org/apache/tajo/engine/codegen/EvalCodeGenerator.java
+++ tajo-core/src/main/java/org/apache/tajo/engine/codegen/EvalCodeGenerator.java
@@ -18,9 +18,7 @@
 
 package org.apache.tajo.engine.codegen;
 
-import org.apache.tajo.catalog.CatalogUtil;
 import org.apache.tajo.catalog.Column;
-import org.apache.tajo.catalog.FunctionDesc;
 import org.apache.tajo.catalog.Schema;
 import org.apache.tajo.common.TajoDataTypes;
 import org.apache.tajo.datum.Datum;
@@ -31,14 +29,10 @@ import org.apache.tajo.org.objectweb.asm.Label;
 import org.apache.tajo.org.objectweb.asm.Opcodes;
 import org.apache.tajo.org.objectweb.asm.Type;
 import org.apache.tajo.storage.Tuple;
-import org.apache.tajo.storage.VTuple;
-import org.apache.tajo.tuple.offheap.HeapTuple;
-import org.apache.tajo.tuple.offheap.HeapTupleBytesComparator;
 
 import java.util.Stack;
 
 import static org.apache.tajo.common.TajoDataTypes.DataType;
-import static org.apache.tajo.engine.codegen.TajoGeneratorAdapter.TUPLE;
 import static org.apache.tajo.engine.codegen.TajoGeneratorAdapter.getDescription;
 import static org.apache.tajo.engine.eval.FunctionEval.ParamType;
 
@@ -636,40 +630,16 @@ public class EvalCodeGenerator extends SimpleEvalNodeVisitor<EvalCodeGenContext>
 
   @Override
   public EvalNode visitFuncCall(EvalCodeGenContext context, FunctionEval func, Stack<EvalNode> stack) {
-    int paramNum = func.getArgs().length;
-    context.push(paramNum);
-    context.newArray(Datum.class); // new Datum[paramNum]
-    final int DATUM_ARRAY = context.astore();
-
-    stack.push(func);
-    EvalNode[] params = func.getArgs();
-    for (int paramIdx = 0; paramIdx < func.getArgs().length; paramIdx++) {
-      context.aload(DATUM_ARRAY);       // array ref
-      context.methodvisitor.visitLdcInsn(paramIdx); // array idx
-      visit(context, params[paramIdx], stack);
-      context.convertToDatum(params[paramIdx].getValueType(), true);  // value
-      context.methodvisitor.visitInsn(Opcodes.AASTORE);
+    if (func.getFuncDesc().getInvocation().hasScalar()) {
+      ScalarFunctionBindingEmitter.emit(this, context, func, stack);
+      return func;
     }
-    stack.pop();
 
-    context.methodvisitor.visitTypeInsn(Opcodes.NEW, TajoGeneratorAdapter.getInternalName(VTuple.class));
-    context.methodvisitor.visitInsn(Opcodes.DUP);
-    context.aload(DATUM_ARRAY);
-    context.newInstance(VTuple.class, new Class[]{Datum[].class});  // new VTuple(datum [])
-    context.methodvisitor.visitTypeInsn(Opcodes.CHECKCAST, TajoGeneratorAdapter.getInternalName(Tuple.class)); // cast to Tuple
-    final int TUPLE = context.astore();
+    if (func.getFuncDesc().getInvocation().hasLegacy()) {
+      LegacyFunctionBindingEmitter.emit(this, context, func, stack);
+      return func;
+    }
 
-    FunctionDesc desc = func.getFuncDesc();
-
-    String fieldName = context.variables.symbols.get(func);
-    String funcDescName = "L" + TajoGeneratorAdapter.getInternalName(desc.getFuncClass()) + ";";
-
-    context.aload(0);
-    context.methodvisitor.visitFieldInsn(Opcodes.GETFIELD, context.owner, fieldName, funcDescName);
-    context.aload(TUPLE);
-    context.invokeVirtual(desc.getFuncClass(), "eval", Datum.class, new Class[] {Tuple.class});
-
-    context.convertToPrimitive(func.getValueType());
     return func;
   }
 
@@ -741,7 +711,7 @@ public class EvalCodeGenerator extends SimpleEvalNodeVisitor<EvalCodeGenContext>
 
   @Override
   protected EvalNode visitCaseWhen(EvalCodeGenContext context, CaseWhenEval caseWhen, Stack<EvalNode> stack) {
-    CaseWhenEmitter.getInstance().emit(this, context, caseWhen, stack);
+    CaseWhenEmitter.emit(this, context, caseWhen, stack);
     return caseWhen;
   }
 
